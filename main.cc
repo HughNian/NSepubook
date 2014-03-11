e9633e0e8dd40c7fbd827e7abf64a3bdd7b5ad97
HughNian <Hugh.Nian@163.com>
2014-03-11 17:34:22 +0800

    commit htmlcxx

---
+++ main.cc
@@ -0,0 +1,22 @@
+/******
+ * NSepubook
+ *
+ * @author: hughnian <hugh.nian@163.com>
+ *
+ *
+ ******/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <lib/htmlcxx/html/ParserDom.h>
+
+using namespace std;
+using namespace htmlcxx;
+
+int main(void)
+{
+    string html;
+    cin>>html;
+
+
+}
